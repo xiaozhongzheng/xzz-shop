47a7ea02a149fe709ede0962410ebf47832d7453
xzz <2975357626@qq.com>
2025-07-29 21:33:27 +0800

    feat：优化虚拟列表

--- src/components/XtxGuess1.vue
+++ src/components/XtxGuess1.vue
@@ -3,138 +3,70 @@
 import { getGoodsGuessListApi } from '@/services/apis/home'
 import type { PageParams } from '@/types/global'
 import type { GuessItem } from '@/types/home'
+import type { GoodsItem } from '@/types/global'
 import { computed, onMounted, reactive, ref } from 'vue'
 import { debounce } from '@/utils'
+import { onShow } from '@dcloudio/uni-app'
 let guessList = ref<GuessItem[]>([])
-const dataList = ref([
-  {
-    id: '4026198',
-    name: '经典格子元素，男童加绒格子衬衫',
-    desc: '经典格子元素，翻盖小贴袋装饰app',
-    price: '125.00',
-    picture: 'https://yanxuan-item.nosdn.127.net/084f82c43a71bbc4d280fbb09a47a028.jpg',
-    orderNum: 53,
-  },
-  {
-    id: '4013563',
-    name: '轻松出行复古经典老花时尚女式钥匙包',
-    desc: '简约潮流女士钥匙包app',
-    price: '88.00',
-    picture: 'https://yanxuan-item.nosdn.127.net/cedfb4827ca489e574abeb5d19ce21e5.jpg',
-    orderNum: 25,
-  },
-  {
-    id: '4007963',
-    name: '新疆棉宝宝时尚涂鸦短袖连体衣59-90cm',
-    desc: '创意手绘图案，透气纯棉面料，3色可选app',
-    price: '69.00',
-    picture: 'https://yanxuan-item.nosdn.127.net/a8b170b3114d33dae0ae98cddb5fb3f6.jpg',
-    orderNum: 2008,
-  },
-  {
-    id: '1113002',
-    name: '谷风一木3层硬抽3盒/提',
-    desc: '一纸三层，吸水耐用不易破app',
-    price: '15.90',
-    picture: 'https://yanxuan-item.nosdn.127.net/0f961546dde35328e7842afe11b98d79.jpg',
-    orderNum: 388,
-  },
-  {
-    id: '1513021',
-    name: '湿水不易破臻护倍韧4层软抽纸巾6包/提',
-    desc: '干湿两用柔韧并济app',
-    price: '29.90',
-    picture: 'https://yanxuan-item.nosdn.127.net/78d56b565a01d241ccae8d38717d3a1e.png',
-    orderNum: 693,
-  },
-  {
-    id: '1113001',
-    name: '谷风一木3层软抽面巾纸6包/提',
-    desc: '进口原木浆和风高颜值app',
-    price: '15.90',
-    picture: 'https://yanxuan-item.nosdn.127.net/f0373e8b2cf9399e53cfac5b2fac0cda.jpg',
-    orderNum: 667,
-  },
-  {
-    id: '1077003',
-    name: '谷风一木3层180g卷纸10卷/提',
-    desc: '4层180g升级款过渡期随机发货！app',
-    price: '27.90',
-    picture: 'https://yanxuan-item.nosdn.127.net/544dc93575aca22f9d3ef614ebf1e362.jpg',
-    orderNum: 352,
-  },
-  {
-    id: '3990408',
-    name: '1滴强力去污澳洲超浓缩不伤手洗洁精400ml',
-    desc: '温和高效去污，一瓶可作多用app',
-    price: '17.90',
-    picture: 'https://yanxuan-item.nosdn.127.net/9ffdc0b1f6dbbe2e9f10a187444c01a3.png',
-    orderNum: 427,
-  },
-  {
-    id: '4005106',
-    name: '新疆棉，儿童色织条纹短袖连体衣59-90cm',
-    desc: '100%棉面料，穿着舒适透气app',
-    price: '59.00',
-    picture: 'https://yanxuan-item.nosdn.127.net/2b3748a78c10027dca96504d05acc4bc.png',
-    orderNum: 1264,
-  },
-  {
-    id: '1343000',
-    name: '最不舍柔情似水的你！平台云音乐3层软抽6包',
-    desc: '我的心意，跃然纸上app',
-    price: '17.90',
-    picture: 'https://yanxuan-item.nosdn.127.net/c43370d1090d445913dcb69a34875a59.jpg',
-    orderNum: 374,
-  },
-])
+
+const dataList = ref<GoodsItem[]>([])
 const totalHeight = ref(0)
 // Reqired 可以将参数变为必选
 let queryParams: Required<PageParams> = {
   page: 1,
-  pageSize: 10,
+  pageSize: 100,
 }
-// let getGuessList = async () => {
-//   let { result } = await getGoodsGuessListApi(queryParams)
-//   guessList.value = result.dataList
-// }
-
+let getGuessList = async () => {
+  let { result } = await getGoodsGuessListApi(queryParams) // 获取数据
+  dataList.value = result.items
+  totalHeight.value = itemHeight * dataList.value.length // 计算滚动区域的总高度
+  console.log(totalHeight.value, 'totalHeight.value')
+  handleScroll()
+}
 onMounted(() => {
-  // getGuessList()
+  getGuessList()
+})
+onShow(() => {
+  // 每次页面展示的时候，都会执行下面的方法
+  getGuessList()
 })
 const itemHeight = 200 // 列表每个项的高度
 const visibleSize = 600 // 可视化区域
-const startIndex = ref(0)
-const endIndex = ref(0)
-const scrollTop = ref(0)
-const startOffset = computed(() => {
-  return startIndex.value * itemHeight
-})
+const startIndex = ref(0) // 可视化区域的开始索引
+const endIndex = ref(0) // 可视化区域结束的索引
+const scrollTop = ref(0) // 容器内滚动条的高度
+
 const scrollRef = ref()
-const showSize = Math.floor(visibleSize / itemHeight) // 可是区域展示item的个数
-console.log()
-onMounted(() => {
-  totalHeight.value = itemHeight * dataList.value.length
-  console.log(totalHeight.value, 'totalHeight.value')
-  // guessList.value = dataList.value
-  handleScroll()
-})
+const showSize = Math.floor(visibleSize / itemHeight) // 可视区域展示item的个数
+
 const handleScroll = () => {
   // console.log(scrollRef.value.$el.scrollTop)
-  const scorllDom = scrollRef.value.$el
-  scrollTop.value = scorllDom.scrollTop * 2 // 获取滚动条滚动的距离
+  const scrollDom = scrollRef.value.$el
+  scrollTop.value = scrollDom.scrollTop * 2 // 获取滚动条滚动的距离
   console.log(scrollTop.value, 'scrollTop')
   startIndex.value = preStartIndex.value
   endIndex.value = nextEndIndex.value
   console.log(startIndex.value, endIndex.value, 'index')
 }
+
+// 使用防抖技术优化滚动条滑动时多次调用方法
+const debounceScroll = debounce(handleScroll, 100)
+
+// 计算偏移距离，用于在可视化区域准确的展示数据
+const startOffset = computed(() => {
+  return startIndex.value * itemHeight
+})
+
+// 在开始索引的基础上 -3，表示向上预加载3条数据，可优化用户快速下滑出现页面白屏的问题
 const preStartIndex = computed(() => {
   let index = Math.floor(scrollTop.value / itemHeight)
-  if (index - 2 > 0) {
-    return index - 2
+  if (index - 3 > 0) {
+    return index - 3
   }
   return index
 })
+
+// 在开始索引的基础上 +3，表示向下预加载3条数据
 const nextEndIndex = computed(() => {
   let index = startIndex.value + showSize
   const lastIndex = dataList.value.length
@@ -142,15 +74,15 @@ const nextEndIndex = computed(() => {
     // 表示没有更多数据展示了
     return dataList.value.length - 1
   }
-  index += 2 // 向下多展示2条数据，防止滑动时出现白屏
-
+  index += 3 // 向下多展示2条数据，防止滑动时出现白屏
   return index > lastIndex ? lastIndex : index
 })
+
+// 可视化区域要展示的数据
 const visibleData = computed(() => {
   // 计算可视区域要展示的数据
   return dataList.value.slice(startIndex.value, endIndex.value)
 })
-const debounceScroll = debounce(handleScroll, 100)
 </script>
 
 <template>
@@ -159,6 +91,7 @@ const debounceScroll = debounce(handleScroll, 100)
     <text class="text">猜你喜欢</text>
   </view>
   <view class="scrollBox" @scroll="debounceScroll" ref="scrollRef">
+    <!-- 重点：使用一个空的盒子撑开父盒子，该盒子的高度取决于所有数据的高度和 -->
     <view
       :style="{
         height: totalHeight + 'rpx',
@@ -178,12 +111,6 @@ const debounceScroll = debounce(handleScroll, 100)
         :url="`/pages/goods/goods?id=${item.id}`"
         :style="{ height: itemHeight + 'rpx' }"
       >
-        <!-- <view
-        class="guess-item"
-        v-for="item in visibleData"
-        :key="item.id"
-        :style="{ height: itemHeight + 'rpx' }"
-      > -->
         <image class="image" mode="aspectFill" :src="item.picture"></image>
         <view>
           <view class="name"> {{ item.name }} </view>
@@ -192,7 +119,6 @@ const debounceScroll = debounce(handleScroll, 100)
             <text>{{ item.price }}</text>
           </view>
         </view>
-        <!-- </view> -->
       </navigator>
     </view>
   </view>
